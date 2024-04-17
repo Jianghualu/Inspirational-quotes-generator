4c7fdffaddb868babbe7336301fbb6d8ddc3b3fb
SarahisonFire <sarahlu8513@gmail.com>
2024-04-16 17:50:33 -0700

    add React Modal components

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -22,3 +22,130 @@ export const GradientBackgroundCon = styled.div`
         }
     }
 `;
+export const BackgroundImageFireWorks1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-top: -10px;
+  margin-left: -10px;
+  opacity: .7;
+`;
+
+export const BackgroundImageFireWorks = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  top: -10px;
+  right: -30px;
+  opacity: .8;
+`;
+
+export const FooterCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: 'Roboto', monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 99999;
+`;
+
+export const FooterLink = styled(Link)`
+  color: white;
+`;
+
+export const QuoteGeneratorCon = styled.div`
+    min-height: 350px;
+    min-width: 350px;
+    height: 70vh;
+    width: 70vw;
+    border: 2px solid #ffffff22;
+    border-radius: 15px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    z-index: 2;
+
+    background: rgba( 190, 185, 138, 0.2 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 1.5px );
+    -webkit-backdrop-filter: blur( 1.5px );
+    border-radius: 10px;
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    width: 100%;
+`;
+
+
+export const QuoteGeneratorTitle = styled.div`
+    font-family: 'Permanent Marker', cursive;
+    font-size: 50px;
+    text-align: center;
+    color: white;
+    padding: 0px 20px 0px 20px;
+    position: relative;
+    /* media query */
+    @media only screen and (max-width: 600px) {
+        font-size: 30px;
+    }
+`;
+
+export const QuoteGeneratorSubTitle = styled.div`
+    color: white;
+    font-family: 'Kalam', cursive;
+    font-size: 35px;
+    position: relative;
+    width: 100%;
+    text-align: center;
+    padding: 0px 20px 0px 20px;
+    @media only screen and (max-width: 600px) {
+        font-size: 25px;
+    }
+`;
+
+export const QuoteButton = styled.div`
+    height: 100px;
+    width: 300px;
+    border: 2px solid darkgrey;
+    border-radius: 20px;
+    
+    margin-top: 20px;
+    position: relative;
+    transition: 0.2s all ease-in-out;
+    cursor: pointer;
+    top: 20px;
+    margin: auto;
+    transform-origin: center;
+    
+    background: rgba( 0, 0, 70, 0.3 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 20px );
+    -webkit-backdrop-filter: blur( 20px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
+    &:hover {
+        filter: brightness(3);
+        transition: 0.2s all ease-in-out;
+        transform: scale(1.1);
+
+        transform-origin: center;
+    }
+`;
+
+export const QuoteButtonText = styled.div`
+    color: white;
+    font-family: 'Kalam', cursive;
+    font-size: 35px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    width: 100%;
+    text-align: center;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
+// Components:
+import {
+  GradientBackgroundCon,
+  BackgroundImageFireWorks1,
+  BackgroundImageFireWorks,
+  FooterCon,
+  FooterLink,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorTitle,
+  QuoteGeneratorSubTitle,
+  QuoteButton,
+  QuoteButtonText,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
-
+//Assets:
+import Moon from "../assets/moon.png";
+import Fireworks from "../assets/fireworks.png";
+import Fireworks1 from "../assets/firework1.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -15,9 +34,64 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* Backgrounds */ }
+      {/* Backgrounds */}
       <GradientBackgroundCon>
+        {/* Quote Generator Modal Pop*/}
+        {/* <QuoteGeneratorModal></QuoteGeneratorModal> */}
 
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspirational Quotes
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Get your quote card with a random inspirational quote provided by {' '}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+            <QuoteButton>
+              <QuoteButtonText onClick={null}>
+                Make a Quote
+              </QuoteButtonText>
+            </QuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        
+        {/* <BackgroundImageMoon src={Moon} height="200" alt="moonbackground" /> */}
+        <BackgroundImageFireWorks
+          src={Fireworks}
+          height="500"
+          alt="fireworksbackground"
+        />
+        <BackgroundImageFireWorks1
+          src={Fireworks1}
+          height="500"
+          alt="fireworksbackground"
+        />
+        {/* Footer */}
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by{" "}
+            <FooterLink
+              href="https://www.linkedin.com/in/jianghua-lu-0310651a8/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @Jianghua Lu
+            </FooterLink>
+          </>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   );
